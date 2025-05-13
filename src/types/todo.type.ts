2ebc891d045425ef679c2edfc99bb4d8b121173b
Young <dndud5182@gmail.com>
2025-05-14 02:25:21 +0900

    Feat : Todo 타입 및 관련 props 인터페이스 정의 추가

--- src/types/todo.type.ts
+++ src/types/todo.type.ts
@@ -1,5 +1,25 @@
+/** 하나의 할 일 항목 정의 */
 export interface Todo {
-    id: string;
-    title: string;
-    completed: boolean;
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+/** 할 일 추가 폼에서 사용하는 props */
+export interface TodoFormProps {
+  onAdd: (title: string) => void;
+}
+
+/** 할 일 리스트에서 사용하는 props */
+export interface TodoListProps {
+  todos: Todo[];
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+}
+
+/** 할 일 아이템에서 사용하는 props */
+export interface TodoItemProps {
+  todo: Todo;
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
 }
