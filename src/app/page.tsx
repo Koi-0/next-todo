9e6f65b15cdf6a79bb8678eef7a24d20668d1e4a
Young <dndud5182@gmail.com>
2025-05-14 03:40:13 +0900

    Feat : HydrationBoundary 추가 및 쿼리 클라이언트 prefetch 로직 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import TodoContainer from "./components/TodoContainer";
+import { getTodos } from "./api/todo-api";
 
 const Home = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
   return (
-    <main className="mx-auto max-w-md space-y-4 bg-red-500 p-4">
-      <h1 className="text-2xl font-bold">📝 Todo List</h1>
-      <TodoContainer />
-    </main>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <main className="mx-auto max-w-md space-y-4 bg-red-500 p-4">
+        <h1 className="text-2xl font-bold">📝 Todo List</h1>
+        <TodoContainer />
+      </main>
+    </HydrationBoundary>
   );
 };
 
